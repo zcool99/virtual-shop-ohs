1e968c84af14384495220f89f50046171245d1e3
Omar Hernandez <omar.hernandez.sarmiento2@vwfs.com>
2021-04-09 16:38:26 -0500

    Routes 1.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/";
 import Confirm from "./components/Confirm/Confirm";
